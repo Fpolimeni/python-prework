f405920ef8a9ae461b55d916d139c2dee0050bd4
fpolimeni <polimenif@outlook.com>
2023-01-23 18:11:15 -0500

    first commit

---
+++ my_prework.py
@@ -0,0 +1,14 @@
+# Question 1:
+#write a function to print "hello_USERRNAME!" USERNAME is the input function
+
+def hello_name(user_name):
+    # your code here
+    pass
+
+# Questions 2
+# Write a python function, first_odds taht prints the odd number from 1-100 and returns nothing
+def first_odds():
+    #your code here
+    pass
+
+# ect ect ect
